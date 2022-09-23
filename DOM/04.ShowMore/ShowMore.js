08709a78ca1b463a02b89996bd54a367d818225f
ViktoR <Kachamakov@abv.bg>
2022-09-23 21:29:08 +0300

    Add new Homework

---
+++ DOM/04.ShowMore/ShowMore.js
@@ -0,0 +1,12 @@
+function showText() {
+    let a = document.getElementById('text');
+    a.style.display = 'inline';
+    let showMoreElement = document.getElementById('more');
+    showMoreElement.style.display = 'none';
+    
+
+    
+   
+    
+
+}
