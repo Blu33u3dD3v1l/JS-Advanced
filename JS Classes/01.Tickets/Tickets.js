95078e0c36ebc39ab9728f5d71af85133dbabda4
ViktoR <Kachamakov@abv.bg>
2022-10-10 20:59:27 +0300

    Solution

---
+++ JS Classes/01.Tickets/Tickets.js
@@ -0,0 +1,36 @@
+function tickets(arr,str){
+
+    class Ticket{
+        constructor(destination, price, status){
+            this.destination = destination;
+            this.price = price;
+            this.status = status;
+        }
+
+        
+    }
+    let result = [];
+    arr.map(el =>{
+      
+       let [destination,price,status] = el.split('|');
+       price = Number(price);
+       result.push(new Ticket(destination,price,status));
+    });
+    
+    return result.sort((a,b) => {
+     
+
+        if(typeof a[str] === 'number'){
+            return a[str] - b[str];
+        }else{
+            return a[str].localeCompare(b[str])
+        }
+    });
+     
+}
+tickets(['Philadelphia|94.20|available',
+'New York City|95.99|available',
+'New York City|95.99|sold',
+'Boston|126.20|departed'],
+'destination'
+);
